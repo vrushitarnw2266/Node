db7251712371f7712b97ec912eb8c9425b547be5
vrushitarnw2266 <vrushita2366@gmail.com>
2026-04-08 11:35:06 +0530

    http server

--- sarikaMa'm/01_http/02_http_main_logic/projects/server.js
+++ sarikaMa'm/01_http/02_http_main_logic/projects/server.js
@@ -32,29 +32,42 @@
 
 const http = require('http');
 const fs = require('fs');
+const path = require('path');
 
-const server = http.createServer((req,res)=>{
-    if(req.url === '/'){
-        fs.readFile('index.html', (err, data) => {
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.write(data);
-            res.end();
-        });
-    } else if(req.url === '/about'){
-        fs.readFile('about.html', (err, data) => {
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.write(data);
-            res.end();
-        });
-    } else{
-        fs.readFile('404.html', (err, data) => {
-            res.writeHead(404, { 'Content-Type': 'text/html' });
-            res.write(data);
-            res.end();
-        });
+const server = http.createServer((req, res) => {
+    let filePath = '';
+    let contentType = 'text/html';
+
+    if (req.url === '/') {
+        filePath = path.join(__dirname, 'index.html');
+    } else if (req.url === '/about') {
+        filePath = path.join(__dirname, 'about.html');
+    } else if (req.url.endsWith('.css')) {
+        filePath = path.join(__dirname, req.url);
+        contentType = 'text/css';
+    } else {
+        filePath = path.join(__dirname, '404.html');
     }
+
+    fs.readFile(filePath, (err, data) => {
+        if (err) {
+            if (err.code === 'ENOENT') {
+                // If 404.html doesn't exist either, send a simple message
+                res.writeHead(404, { 'Content-Type': 'text/html' });
+                res.end('<h1>404 Not Found</h1>', 'utf-8');
+            } else {
+                res.writeHead(500);
+                res.end(`Server Error: ${err.code}`);
+            }
+        } else {
+            res.writeHead(req.url === '/' || req.url === '/about' ? 200 : (req.url.endsWith('.css') ? 200 : 404), { 'Content-Type': contentType });
+            res.end(data, 'utf-8');
+        }
+    });
 });
-server.listen(3000, () => {
-    console.log("Server running at http://localhost:3000");
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
 });
 
