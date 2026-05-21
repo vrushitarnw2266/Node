160003a8533e32b769343c055eaf82ce2d9f5e45
vrushitarnw2266 <vrushita2366@gmail.com>
2026-05-21 23:40:21 +0530

    feat(new):food ordering

--- sarikaMaM/FOOD_ORDER_WEB/server/index.js
+++ sarikaMaM/FOOD_ORDER_WEB/server/index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin: process.env.CLIENT_URL || true,
     credentials: true,
   })
 );
